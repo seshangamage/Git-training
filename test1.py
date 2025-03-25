1c198a63311ced4ee54ab613af176b4753b02367
Sheshan <seshangamage@gmail.com>
2025-03-25 09:52:02 +0530

    Adding J-1234 new methods

--- test1.py
+++ test1.py
@@ -2,5 +2,13 @@ def sum(a,b):
     sum = a+b
     return sum
 
+def multiply(a,b):
+    multiply = a*b
+    return multiply
+
+def divide(a,b):
+    divide = a/b
+    return divide
+
 sum = sum(1,2)
 print(sum)
