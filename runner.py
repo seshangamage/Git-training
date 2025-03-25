b082e6a08412bdcc448b2fdda5fba7b56e3bb6f1
Sheshan <sheshan@wso2telco.com>
2025-03-25 10:50:02 +0530

    Update code from git runner.py
    Update code from git runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,11 @@
 from math_logic import sum ,  multiply, divide, find_max, find_min
 
 def main():
-    ans = multiply(1, 2)
-    print(ans)
-    ans = divide(4, ans)
-    print(ans)
+    ans1 = multiply(1, 2)
+    print(ans1)
+    ans2 = divide(4, ans1)
+    print(ans2)
+    ans3 =find_max(ans2,ans1)
+    print(ans3)
 
 main()
